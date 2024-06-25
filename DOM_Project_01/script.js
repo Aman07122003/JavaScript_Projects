4fcce54132f2af771c713fd0da145970d622b321
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-06-25 13:08:11 +0000

    full code

--- DOM_Project_01/script.js
+++ DOM_Project_01/script.js
@@ -1,1 +1,19 @@
-console.log("aman")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        const color = e.target.id;
+        body.style.backgroundColor = color;
+
+        // Reset text color to black for all buttons except 'black', 'blue', and 'purple'
+        if (color === 'black' || color === 'blue' || color === 'purple') {
+            body.style.color = 'white';
+        } else {
+            body.style.color = 'black';
+        }
+    });
+});
+
+
+
